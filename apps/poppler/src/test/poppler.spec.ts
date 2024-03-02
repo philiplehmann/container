e4d6908264034bd905630e5d136cfcfe3a81cc08
Philip Lehmann <philiplehmann@gmail.com>
2024-03-02 14:01:51 +0100

    add build and multi-arch tests

--- apps/poppler/src/test/poppler.spec.ts
+++ apps/poppler/src/test/poppler.spec.ts
@@ -6,52 +6,56 @@ import { describe, beforeAll, afterAll, it, expect } from "vitest";
 const containerPort = 5000;
 
 describe("poppler", () => {
-	let container: StartedTestContainer;
-	let port: number;
+	["amd", "arm"].map((arch) => {
+		describe(`arch: ${arch}`, () => {
+			let container: StartedTestContainer;
+			let port: number;
 
-	beforeAll(async () => {
-		container = await new GenericContainer(
-			"philiplehmann/poppler-server:latest",
-		)
-			.withEnvironment({ PORT: String(containerPort) })
-			.withExposedPorts(containerPort)
-			.start();
+			beforeAll(async () => {
+				container = await new GenericContainer(
+					`philiplehmann/poppler-server:test-${arch}`,
+				)
+					.withEnvironment({ PORT: String(containerPort) })
+					.withExposedPorts(containerPort)
+					.start();
 
-		port = container.getMappedPort(containerPort);
-	});
+				port = container.getMappedPort(containerPort);
+			});
 
-	afterAll(async () => {
-		await container.stop();
-	});
+			afterAll(async () => {
+				await container.stop();
+			});
 
-	it("should convert PDF to text", async () => {
-		const file = resolve(__dirname, "assets/dummy.pdf");
-		const [response, text] = await testRequest({
-			method: "POST",
-			host: "localhost",
-			port,
-			path: "/pdf-to-text",
-			headers: { "Content-Type": "application/pdf" },
-			file,
+			it("should convert PDF to text", async () => {
+				const file = resolve(__dirname, "assets/dummy.pdf");
+				const [response, text] = await testRequest({
+					method: "POST",
+					host: "localhost",
+					port,
+					path: "/pdf-to-text",
+					headers: { "Content-Type": "application/pdf" },
+					file,
+				});
+
+				expect(response.statusCode).toBe(200);
+				expect(text).toContain("Dummy PDF file");
+			});
+
+			it('should convert PDF to HTML and include "Dummy PDF file"', async () => {
+				const file = resolve(__dirname, "assets/dummy.pdf");
+				const [response, text] = await testRequest({
+					method: "POST",
+					host: "localhost",
+					port,
+					path: "/pdf-to-html",
+					headers: { "content-type": "application/x-www-form-urlencoded" },
+					file,
+				});
+
+				expect(response.statusCode).toBe(200);
+				expect(text).toContain("Dummy PDF file");
+				expect(text.toLowerCase()).toContain("<!doctype html>");
+			});
 		});
-
-		expect(response.statusCode).toBe(200);
-		expect(text).toContain("Dummy PDF file");
-	});
-
-	it('should convert PDF to HTML and include "Dummy PDF file"', async () => {
-		const file = resolve(__dirname, "assets/dummy.pdf");
-		const [response, text] = await testRequest({
-			method: "POST",
-			host: "localhost",
-			port,
-			path: "/pdf-to-html",
-			headers: { "content-type": "application/x-www-form-urlencoded" },
-			file,
-		});
-
-		expect(response.statusCode).toBe(200);
-		expect(text).toContain("Dummy PDF file");
-		expect(text.toLowerCase()).toContain("<!doctype html>");
 	});
 });
--- apps/tesseract/src/test/tesseract.spec.ts
+++ apps/tesseract/src/test/tesseract.spec.ts
@@ -9,43 +9,49 @@ const expectText =
 	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 describe("tesseract", () => {
-	let container: StartedTestContainer;
-	let port: number;
+	["amd", "arm"].map((arch) => {
+		describe(`arch: ${arch}`, () => {
+			let container: StartedTestContainer;
+			let port: number;
 
-	beforeAll(async () => {
-		container = await new GenericContainer("philiplehmann/tesseract:latest")
-			.withEnvironment({ PORT: String(containerPort) })
-			.withExposedPorts(containerPort)
-			.start();
+			beforeAll(async () => {
+				container = await new GenericContainer(
+					`philiplehmann/tesseract:test${arch}`,
+				)
+					.withEnvironment({ PORT: String(containerPort) })
+					.withExposedPorts(containerPort)
+					.start();
 
-		port = container.getMappedPort(containerPort);
-	});
-
-	afterAll(async () => {
-		await container.stop();
-	});
-
-	for (const type of ["gif", "jpg", "png", "tiff", "webp"]) {
-		it(`should convert ${type} to text`, async () => {
-			const file = resolve(__dirname, `assets/dummy_image.${type}`);
-			const [response, text] = await testRequest({
-				method: "POST",
-				host: "localhost",
-				port,
-				path: "/image-to-text",
-				headers: { "Content-Type": `image/${type}` },
-				file,
+				port = container.getMappedPort(containerPort);
 			});
 
-			expect(response.statusCode).toBe(200);
-			expect(
-				text
-					.split("\n")
-					.join(" ")
-					.replace("|psum", "Ipsum")
-					.replace("lpsum", "Ipsum")
-					.trim(),
-			).toBe(expectText);
+			afterAll(async () => {
+				await container.stop();
+			});
+
+			for (const type of ["gif", "jpg", "png", "tiff", "webp"]) {
+				it(`should convert ${type} to text`, async () => {
+					const file = resolve(__dirname, `assets/dummy_image.${type}`);
+					const [response, text] = await testRequest({
+						method: "POST",
+						host: "localhost",
+						port,
+						path: "/image-to-text",
+						headers: { "Content-Type": `image/${type}` },
+						file,
+					});
+
+					expect(response.statusCode).toBe(200);
+					expect(
+						text
+							.split("\n")
+							.join(" ")
+							.replace("|psum", "Ipsum")
+							.replace("lpsum", "Ipsum")
+							.trim(),
+					).toBe(expectText);
+				});
+			}
 		});
-	}
+	});
 });
