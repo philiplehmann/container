cd9d2a64ddd45fd55f20c590a735615691c0ad70
Philip Lehmann <philip.lehmann@healthadvisor.ch>
2024-04-25 08:11:48 +0200

    Update dependencies and add Playwright plugin for e2e testing

--- apps/puppeteer/vite.config.ts
+++ apps/puppeteer/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     globals: true,
     cacheDir: '../../node_modules/.vitest',
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     testTimeout: 60_000,
 
     reporters: ['default'],
