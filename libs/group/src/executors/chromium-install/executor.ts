1fe7cfbdafe543e8a32c5072bed0837036951acd
Philip Lehmann <philiplehmann@gmail.com>
2024-05-06 06:14:17 +0200

    convert pdf with imagemagick to png

--- libs/group/src/executors/chromium-install/executor.ts
+++ libs/group/src/executors/chromium-install/executor.ts
@@ -3,6 +3,26 @@ import type { ChromiumInstallExecutorSchema } from './schema';
 import type { Executor } from '@nx/devkit';
 
 const runExecutor: Executor<ChromiumInstallExecutorSchema> = async () => {
+  if (process.env.RUNNER_OS === 'Linux') {
+    try {
+      await promiseSpawn('sudo', ['apt-get', 'install', 'ghostscript']);
+      await promiseSpawn('sudo',
+        ['sed', '-i', 's/$SRC/$RPL/', '/etc/ImageMagick-6/policy.xml'],
+        {
+          env: {
+            DQT: '"',
+            SRC: 'rights=${DQT}none${DQT} pattern=${DQT}PDF${DQT}',
+            RPL: 'rights=${DQT}read\|write${DQT} pattern=${DQT}PDF${DQT}'
+          }
+        }
+      );
+    } catch (error) {
+      console.error('could not install ghostscript and imagemagick', error);
+      return {
+        success: false,
+      };
+    }
+  }
   try {
     await promiseSpawn('yarn', ['playwright', 'install', '--with-deps', 'chromium']);
     return {
--- libs/http/body/src/lib/http-body.ts
+++ libs/http/body/src/lib/http-body.ts
@@ -1,32 +1,36 @@
 import type { IncomingMessage } from 'node:http';
 import { BadRequest } from '@container/http/error';
 import { z } from 'zod';
+import { Readable } from 'node:stream';
 
 const applicationJSON = z.object({
   'content-type': z.literal('application/json'),
 });
 
-export const requestToBuffer = async (req: IncomingMessage): Promise<Buffer> => {
+export const streamToBuffer = async (req: Readable): Promise<Buffer> => {
   const bodyParts: Buffer[] = [];
   for await (const chunk of req) {
     bodyParts.push(chunk);
   }
   return Buffer.concat(bodyParts);
+}
+
+export const requestToBuffer = async (req: IncomingMessage): Promise<Buffer> => {
+  return streamToBuffer(req);
 };
 
-export const requestToText = async (req: IncomingMessage): Promise<string> => {
-  const buffer = await requestToBuffer(req);
+export const streamToText = async (req: Readable): Promise<string> => {
+  const buffer = await streamToBuffer(req);
   return buffer.toString('utf-8');
 };
 
-export const requestToJson = async <T = unknown>(req: IncomingMessage): Promise<T> => {
-  const test = applicationJSON.safeParse(req.headers);
-  if (test.success === false) {
-    throw new BadRequest(JSON.stringify(test.error));
-  }
+export const requestToText = async (req: IncomingMessage): Promise<string> => {
+  return streamToText(req);
+};
 
+export const streamToJson = async <T = unknown>(req: Readable): Promise<T> => {
   try {
-    const body = await requestToText(req);
+    const body = await streamToText(req);
     return JSON.parse(body);
   } catch (error) {
     if (error instanceof Error) {
@@ -37,4 +41,13 @@ export const requestToJson = async <T = unknown>(req: IncomingMessage): Promise<
     }
     throw new BadRequest('Unknown error');
   }
+}
+
+export const requestToJson = async <T = unknown>(req: IncomingMessage): Promise<T> => {
+  const test = applicationJSON.safeParse(req.headers);
+  if (test.success === false) {
+    throw new BadRequest(JSON.stringify(test.error));
+  }
+
+  return streamToJson(req);
 };
