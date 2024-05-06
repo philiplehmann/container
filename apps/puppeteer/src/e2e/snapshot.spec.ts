d72abd2dfecf8360295bdb76ad0ab23f27700783
Philip Lehmann <philiplehmann@gmail.com>
2024-05-06 06:14:17 +0200

    convert pdf with imagemagick to png

--- apps/puppeteer/src/e2e/snapshot.spec.ts
+++ apps/puppeteer/src/e2e/snapshot.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
-import { GenericContainer, type StartedTestContainer, Network, type StartedNetwork } from 'testcontainers';
-import { streamRequest, testRequest } from '@container/test/request';
-import { currentArch } from '@container/docker';
-import { unlink } from 'node:fs/promises';
-import { resolve } from 'node:path';
-import { createWriteStream } from 'node:fs';
+import { GenericContainer, type StartedTestContainer } from 'testcontainers';
+import { streamRequest } from '@container/test/request';
+import { currentArch, promiseSpawn } from '@container/docker';
+import { readdir, unlink } from 'node:fs/promises';
+import { join, resolve } from 'node:path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { streamToBuffer } from '@container/http/body';
+import { Readable } from 'node:stream';
 
 const containerPort = 5000;
 
 let container: StartedTestContainer;
-let nginx: StartedTestContainer;
 let port: number;
-let nginxPort: number;
 
 [currentArch()].map((arch) => {
   test.beforeAll(async () => {
@@ -20,24 +20,17 @@ let nginxPort: number;
       .withExposedPorts(containerPort)
       .withLogConsumer((stream) => stream.pipe(process.stdout))
       .start();
-    nginx = await new GenericContainer('nginx')
-      .withExposedPorts(80)
-      .withLogConsumer((stream) => stream.pipe(process.stdout))
-      .withBindMounts([{ source: resolve(__dirname, 'assets'), target: '/usr/share/nginx/html' }])
-      .start();
 
     port = container.getMappedPort(containerPort);
-    nginxPort = nginx.getMappedPort(80);
-    process.env.BASE_URL = `http://localhost:${nginxPort}`;
   });
 
   test.afterAll(async () => {
     await container.stop();
-    await nginx.stop();
   });
 
   let numberOfPages = 0;
-  let fileName = '';
+  let filePath = '';
+  let outputPaths: string[] = [];
 
   test.beforeEach(async ({ page, browserName }) => {
     const response = await streamRequest({
@@ -55,26 +48,28 @@ let nginxPort: number;
         margin: { top: 0, right: 0, bottom: 0, left: 0 },
       }),
     });
-    fileName = `${Date.now()}.pdf`;
-    const path = resolve(__dirname, 'assets', fileName);
+    const date = String(Date.now());
+    const fileName = `${date}.pdf`;
+    filePath = resolve(__dirname, 'assets', fileName);
 
-    response.pipe(createWriteStream(path), { end: true });
+    response.pipe(createWriteStream(filePath), { end: true });
 
-    await page.goto(`http://localhost:${nginxPort}/pdf.html?file=${encodeURIComponent(fileName)}`);
-    await page.waitForFunction(() => typeof print === 'object');
-    numberOfPages = await page.evaluate(async () => print.numPages);
+    const imagePath = filePath.replace(/pdf$/, 'png')
+    await promiseSpawn('convert', [filePath, imagePath]);
+    const files = await readdir(resolve(__dirname, 'assets'));
+    outputPaths = files.filter((file) => file.endsWith('.png') && file.startsWith(date)).map((path) => join(resolve(__dirname, 'assets'), path))
   });
 
   test.afterEach(async () => {
-    await unlink(resolve(__dirname, 'assets', fileName));
+    await Promise.all([filePath, ...outputPaths].map(async (path) => {
+      await unlink(path);
+    }));
   });
 
   test('test pdf visually', async ({ page }) => {
-    test.setTimeout(120000);
-    const theCanvas = page.locator('#the-canvas');
-    for (let i = 0; i < numberOfPages; i++) {
-      expect(await theCanvas.screenshot()).toMatchSnapshot({ name: `page-${i}.png` });
-      await page.evaluate(() => print.nextPage());
-    }
+    await Promise.all(outputPaths.map(async (imagePath, index) => {
+      const buffer = await streamToBuffer(Readable.from(createReadStream(imagePath)));
+      expect(buffer).toMatchSnapshot({ name: `page-${index}.png`, maxDiffPixelRatio: 0.02 });
+    }));
   });
 });
