2491348e668cce6c496cf8125fc97db1f22b4ed2
Philip Lehmann <philiplehmann@gmail.com>
2024-05-06 07:04:15 +0200

    show convert version and config

--- libs/helper/src/executors/e2e-install/executor.ts
+++ libs/helper/src/executors/e2e-install/executor.ts
@@ -6,16 +6,15 @@ const runExecutor: Executor<E2EInstallExecutorSchema> = async () => {
   if (process.env.RUNNER_OS === 'Linux') {
     try {
       await promiseSpawn('sudo', ['apt-get', 'install', 'ghostscript', '--yes', '--no-install-recommends']);
-      await promiseSpawn('sudo',
-        ['sed', '-i', 's/$SRC/$RPL/', '/etc/ImageMagick-6/policy.xml'],
-        {
-          env: {
-            DQT: '"',
-            SRC: 'rights=${DQT}none${DQT} pattern=${DQT}PDF${DQT}',
-            RPL: 'rights=${DQT}read\|write${DQT} pattern=${DQT}PDF${DQT}'
-          }
-        }
-      );
+      await promiseSpawn('sudo', ['sed', '-i', 's/$SRC/$RPL/', '/etc/ImageMagick-6/policy.xml'], {
+        env: {
+          DQT: '"',
+          SRC: 'rights=${DQT}none${DQT} pattern=${DQT}PDF${DQT}',
+          RPL: 'rights=${DQT}read|write${DQT} pattern=${DQT}PDF${DQT}',
+        },
+      });
+      await promiseSpawn('convert', ['--version']);
+      await promiseSpawn('sudo', ['cat', '/etc/ImageMagick-6/policy.xml']);
     } catch (error) {
       console.error('could not install ghostscript and imagemagick', error);
       return {
