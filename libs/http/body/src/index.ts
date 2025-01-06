ca1231caf999363dd04a979072c982c2b5440ff7
Philip Lehmann <philiplehmann@gmail.com>
2025-01-06 21:43:38 +0100

    code review changes

--- libs/http/body/src/index.ts
+++ libs/http/body/src/index.ts
@@ -3,4 +3,4 @@ export * from './lib/request-to-buffer';
 export * from './lib/request-to-json';
 export * from './lib/request-to-text';
 export * from './lib/streamable-file';
-export * from './lib/check-content-type';
+export * from './lib/validate-request-headers';
--- libs/http/body/src/lib/request-to-multipart-form-data/get-content-disposition-name.ts
+++ libs/http/body/src/lib/request-to-multipart-form-data/get-content-disposition-name.ts
@@ -1,13 +1,20 @@
 export function getContentDispositionName(header: Headers): { filename?: string; name: string } {
+  const contentDisposition = header.get('content-disposition');
+  if (!contentDisposition) {
+    throw new Error('Missing content-disposition header');
+  }
+
   const { filename, name } = Object.fromEntries(
-    header
-      .get('content-disposition')
-      ?.split(';')
-      .map((part) => {
-        const [key, value = ''] = part.trim().split('=');
-        return [key, value.replace(/^"(.*)"$/, '$1')];
-      }) ?? [],
+    contentDisposition.split(';').map((part) => {
+      const [key, value = ''] = part.trim().split('=');
+      return [key, value.replace(/^"(.*)"$/, '$1')];
+    }),
   );
+
+  if (!name) {
+    throw new Error('Missing name in content-disposition header');
+  }
+
   return {
     filename,
     name,
