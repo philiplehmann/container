fabadc0a4cce1fc089174e33bd3f0c547540e37f
Philip Lehmann <philip.lehmann@healthadvisor.ch>
2025-03-23 15:21:38 +0100

    Refactor Docker executor to use dockerfile variable for version retrieval

--- libs/docker/src/executors/build/executor.ts
+++ libs/docker/src/executors/build/executor.ts
@@ -4,30 +4,30 @@ import { createTags, isAutoTags, versionFromEnv, versionFromPackageJson, version
 import type { DockerBuildExecutorSchema } from './schema';
 
 const runExecutor: Executor<DockerBuildExecutorSchema> = async (
-  { file, tags = [], platforms },
+  { file: dockerfile, tags = [], platforms },
   { projectName, projectGraph },
 ) => {
   if (isAutoTags(tags)) {
     switch (projectName) {
       case 'mailcatcher':
-        tags = createTags(tags, versionFromEnv(file, 'MAILCATCHER_VERSION'));
+        tags = createTags(tags, versionFromEnv(dockerfile, 'MAILCATCHER_VERSION'));
         break;
       case 'maildev':
         tags = createTags(
           tags,
-          versionFromEnv(file, 'MAILDEV_VERSION', (v) => v.replace('v', '')),
+          versionFromEnv(dockerfile, 'MAILDEV_VERSION', (v) => v.replace('v', '')),
         );
         break;
       case 'mailhog':
         tags = createTags(
           tags,
-          versionFromEnv(file, 'MAILHOG_VERSION', (v) => v.replace('v', '')),
+          versionFromEnv(dockerfile, 'MAILHOG_VERSION', (v) => v.replace('v', '')),
         );
         break;
       case 'poppler':
         tags = createTags(
           tags,
-          versionFromEnv(file, 'POPPLER_VERSION', (v) => v.split('-').shift() ?? ''),
+          versionFromEnv(dockerfile, 'POPPLER_VERSION', (v) => v.split('-').shift() ?? ''),
         );
         break;
       case 'puppeteer':
@@ -36,19 +36,19 @@ const runExecutor: Executor<DockerBuildExecutorSchema> = async (
       case 'tesseract':
         tags = createTags(
           tags,
-          versionFromEnv(file, 'TESSERACT_VERSION', (v) => v.split('-').shift() ?? ''),
+          versionFromEnv(dockerfile, 'TESSERACT_VERSION', (v) => v.split('-').shift() ?? ''),
         );
         break;
       case 'unoserver':
-        tags = createTags(tags, versionFromEnv(file, 'UNOSERVER_VERSION'));
+        tags = createTags(tags, versionFromEnv(dockerfile, 'UNOSERVER_VERSION'));
         break;
       case 'pdftk':
-        tags = createTags(tags, versionFromEnv(file, 'PDFTK_VERSION'));
+        tags = createTags(tags, versionFromEnv(dockerfile, 'PDFTK_VERSION'));
         break;
       case 'nx-cache-server':
         tags = createTags(tags, versionFromEnv(file, 'NX_CACHE_SERVER_VERSION'));
       case 'easyocr':
-        tags = createTags(tags, versionFromRequirements(file, 'EASYOCR_VERSION'));
+        tags = createTags(tags, versionFromRequirements(dockerfile, 'easyocr'));
         break;
     }
   }
@@ -56,7 +56,7 @@ const runExecutor: Executor<DockerBuildExecutorSchema> = async (
     await dockerBuildxBuild({
       platforms: platforms,
       output: 'push',
-      file,
+      file: dockerfile,
       tags: tags,
     });
     return {
