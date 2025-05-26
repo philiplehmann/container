520b68b24833937436ad9602103bd922cc1d0fbb
Philip Lehmann <philip.lehmann@healthadvisor.ch>
2025-05-26 21:07:17 +0200

    Add docker container and image prune functions and integrate them into test container setup

--- libs/docker/src/index.ts
+++ libs/docker/src/index.ts
@@ -2,4 +2,7 @@ export * from './docker-buildx-build';
 export * from './docker-helper';
 export * from './docker-image-remove';
 export * from './docker-run';
+export * from './docker-container-prune';
+export * from './docker-image-remove';
+export * from './docker-image-prune';
 export * from './promise-spawn';
