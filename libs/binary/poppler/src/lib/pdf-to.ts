7751988365d608a5af58970a5629417f3896070f
Philip Lehmann <philip.lehmann@healthadvisor.ch>
2025-07-09 06:20:36 +0200

    Update biome schema version and improve error logging in route handling

--- libs/binary/poppler/src/lib/pdf-to.ts
+++ libs/binary/poppler/src/lib/pdf-to.ts
@@ -3,9 +3,17 @@ import type { Writable } from 'node:stream';
 import { type InputType, streamChildProcess, streamChildProcessToBuffer } from '@container/stream';
 import type { ConvertTo } from './convert-to';
 
-export function pdfTo(options: { input: InputType; output: Writable; to: ConvertTo }): void;
+export function pdfTo(options: { input: InputType; output: Writable; to: ConvertTo }): Promise<void>;
 export function pdfTo(options: { input: InputType; to: ConvertTo }): Promise<Buffer>;
-export function pdfTo({ input, output, to }: { input: InputType; output?: Writable; to: ConvertTo }) {
+export function pdfTo({
+  input,
+  output,
+  to,
+}: {
+  input: InputType;
+  output?: Writable;
+  to: ConvertTo;
+}): Promise<void> | Promise<Buffer> {
   const command = (() => {
     switch (to) {
       case 'text':
